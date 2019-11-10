30f9340418a1f1abd5d69a19e55a3a0a53480cf7
filafactum <56085801+filafactum@users.noreply.github.com>
2019-11-11 00:09:05 +0100

    add scrollbutton

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
+
+// hamburger menu
 console.log("Hi, welcome to my design universe!")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
 }
+
+
+// back to top button
+var btn = $('#button-top')
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show')
+  } else {
+    btn.removeClass('show')
+  }
+})
+
+btn.on('click', function (e) {
+  e.preventDefault()
+  $('html, body').animate({ scrollTop: 0 }, '500')
+})
