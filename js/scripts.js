39028e26df76c298c3e4460831bb5dff1356d6f0
filafactum <56085801+filafactum@users.noreply.github.com>
2019-12-12 23:49:48 +0100

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 console.log('Hi, welcome to my design universe!')
 
 // toggle menu
-function menuToggle () {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
@@ -14,7 +14,7 @@ function menuToggle () {
 // back to top button
 var btn = $('#button-top')
 
-$(window).scroll(function () {
+$(window).scroll(function() {
   if ($(window).scrollTop() > 500) {
     btn.addClass('show')
   } else {
@@ -22,9 +22,20 @@ $(window).scroll(function () {
   }
 })
 
-btn.on('click', function (e) {
+btn.on('click', function(e) {
   e.preventDefault()
   $('html, body').animate({
     scrollTop: 0
   }, '500')
 })
+
+// scroll to contact
+$('a[href^="#contact"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top
+    }, 1000);
+  }
+});
